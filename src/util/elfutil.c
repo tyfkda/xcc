67655c4109216df939fb7f6fcc5196aadec01059
tyfkda <tyfkda@gmail.com>
2020-04-25 20:54:10 +0900

    Fix type

--- src/util/elfutil.c
+++ src/util/elfutil.c
@@ -39,7 +39,7 @@ void out_elf_header(FILE* fp, uintptr_t entry, int phnum) {
 }
 
 void out_program_header(FILE* fp, int sec, uintptr_t offset, uintptr_t vaddr,
-                        uintptr_t filesz, uintptr_t memsz) {
+                        size_t filesz, size_t memsz) {
   static const int kFlags[] = {
     PF_R | PF_X,  // code
     PF_R | PF_W,  // rwdata
--- src/util/elfutil.h
+++ src/util/elfutil.h
@@ -15,6 +15,6 @@
 
 void out_elf_header(FILE* fp, uintptr_t entry, int phnum);
 void out_program_header(FILE* fp, int sec, uintptr_t offset, uintptr_t vaddr,
-                        uintptr_t filesz, uintptr_t memsz);
+                        size_t filesz, size_t memsz);
 
 #endif  // !ELF_NOT_SUPPORTED
