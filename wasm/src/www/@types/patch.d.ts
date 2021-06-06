a85861a5bdef3507905f6ef16af4cae56083f106
tyfkda <tyfkda@gmail.com>
2021-06-06 11:30:37 +0900

    Save to/load from local file
    File System Access API.

--- wasm/src/www/@types/patch.d.ts
+++ wasm/src/www/@types/patch.d.ts
@@ -4,4 +4,24 @@ declare let ace: any
 
 interface Window {
   initialData: any
+  showOpenFilePicker(option?: any): Promise<[FileSystemFileHandle]>
+  showSaveFilePicker(option?: any): Promise<FileSystemFileHandle>
+}
+
+interface FileSystemHandle {
+  readonly kind: string
+  readonly name: string
+}
+
+interface FileSystemFileHandle extends FileSystemHandle {
+  getFile(): Promise<File>
+  createWritable(): Promise<FileSystemWritableFileStream>
+}
+
+interface WritableStream {
+  close(): any
+}
+
+interface FileSystemWritableFileStream extends WritableStream {
+  write(content: any): Promise<any>
 }
