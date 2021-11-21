c0626ba8170acffdea56797c6fe52ed35e9ca969
tyfkda <tyfkda@gmail.com>
2021-11-21 09:08:48 +0900

    Make assert as an expression

--- inc/assert.h
+++ inc/assert.h
@@ -5,5 +5,5 @@
 #else
 extern int __assert_failed(const char *assertion, const char *fn, int lineno);
 
-#define assert(x)  do { if (!(x)) __assert_failed(#x, __FILE__, __LINE__); } while(0)
+#define assert(x)  ((x) || __assert_failed(#x, __FILE__, __LINE__))
 #endif
