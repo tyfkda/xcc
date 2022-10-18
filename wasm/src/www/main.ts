a751569e929be5556fad2c5034ec3b249735605f
tyfkda <tyfkda@gmail.com>
2022-10-18 11:29:48 +0900

    !==

--- wasm/src/www/main.ts
+++ wasm/src/www/main.ts
@@ -244,7 +244,7 @@ async function run(argStr: string, compileAndDump: boolean) {
   args.unshift('a.wasm')
   try {
     const result = await waproc.runWasmEntry(compiledCode, '_start', args)
-    if (result != 0)
+    if (result !== 0)
       console.error(`Exit code=${result}`)
   } catch (e) {
     if (!(e instanceof ExitCalledError))
