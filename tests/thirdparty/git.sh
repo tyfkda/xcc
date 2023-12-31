30dbab5bf53bcf7b8c93b63c8f509316d667618b
tyfkda <tyfkda@gmail.com>
2023-12-31 15:04:30 +0900

    Update git version
    Set `__STDC_VERSION__=199901L` to pretend modern C compiler.

--- tests/thirdparty/git.sh
+++ tests/thirdparty/git.sh
@@ -3,13 +3,9 @@ set -e
 
 repo='https://github.com/git/git.git'
 . tests/thirdparty/thirdpartycommon
-# git reset --hard 54e85e7af1ac9e9a92888060d6811ae767fea1bc  # 2020/9/9
-git reset --hard 5fbd2fc5997dfa4d4593a862fe729b1e7a89bcf8  # 2021/11/4
+git reset --hard 564d0252ca632e0264ed670534a51d18a689ef5d  # 2023/11/20 v2.43.0
 
-# CFLAGS="-D__STDC_VERSION__=199901L -Drestrict="
-
-# zlib removes `const` using macro if the compiler is not `STDC`
-CFLAGS="-DSTDC"
+CFLAGS="-D__STDC_VERSION__=199901L"
 
 $make clean
 $make V=1 CC="$xcc $CFLAGS"
