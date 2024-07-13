35474c36d9b4d23ba627ef91d87edba99981a5eb
tyfkda <tyfkda@gmail.com>
2024-07-13 16:18:18 +0900

    Handle relocation for the target architecture only

--- include/inttypes.h
+++ include/inttypes.h
@@ -5,12 +5,18 @@
 
 #if defined(__ILP32__)
 
+#define PRId32  "ld"
+#define PRIu32  "lu"
+#define PRIx32  "lx"
 #define PRId64  "lld"
 #define PRIu64  "llu"
 #define PRIx64  "llx"
 
 #elif defined(__LP64__)
 
+#define PRId32  "d"
+#define PRIu32  "u"
+#define PRIx32  "x"
 #define PRId64  "ld"
 #define PRIu64  "lu"
 #define PRIx64  "lx"
