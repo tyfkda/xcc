f0a0e053b1edcbbb4127ca313d456dc0e4352f2a
tyfkda <tyfkda@gmail.com>
2024-07-24 09:44:20 +0900

    修正

--- tests/thirdparty/lua.sh
+++ tests/thirdparty/lua.sh
@@ -6,10 +6,10 @@ repo='https://github.com/lua/lua.git'
 git reset --hard 6443185167c77adcc8552a3fee7edab7895db1a9  # v5.4.6
 
 CFLAGS="-Dl_signalT=int"
-sed -i.bak 's/-DLUA_USE_LINUX//' makefile
+sed -i.bak 's/-DLUA_USE_LINUX//; s/-DLUA_USE_READLINE//; s/-Wl,-E//' makefile
 
 $make clean
 $make -j1 CC="$xcc $CFLAGS"
 
-sed -i 's/ and when == "absent"//' testes/attrib.lua
+sed -i.bak 's/ and when == "absent"//' testes/attrib.lua
 cd testes && ../lua all.lua
