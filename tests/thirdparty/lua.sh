ec60d32a5389ef3fe17b90f740aad050b3b3234f
tyfkda <tyfkda@gmail.com>
2024-07-25 09:49:42 +0900

    LUA_USE_LINUXを有効にしてないとテストに失敗するっぽい
    main.lua(93)あたり、
    他多数。

--- tests/thirdparty/lua.sh
+++ tests/thirdparty/lua.sh
@@ -3,7 +3,7 @@ set -e
 
 repo='https://github.com/lua/lua.git'
 . tests/thirdparty/thirdpartycommon
-git reset --hard 6443185167c77adcc8552a3fee7edab7895db1a9  # v5.4.6
+# git reset --hard 6443185167c77adcc8552a3fee7edab7895db1a9  # v5.4.6
 
 CFLAGS="-Dl_signalT=int"
 sed -i.bak 's/-DLUA_USE_LINUX//; s/-DLUA_USE_READLINE//; s/-Wl,-E//' makefile
