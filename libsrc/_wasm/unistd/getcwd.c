57b396f582489056c7c12c46cdeae63d3fcabb42
tyfkda <tyfkda@gmail.com>
2025-08-06 22:24:16 +0900

    cwdを環境変数PWDから取得するには、constructorの優先度の指定が必要
    同じオブジェクトファイル内だけじゃなく、
    別オブジェクトでもソートする必要がある。
    
    RISCVでは `.init_array.00101`などのセクションになり、
    オブジェクトファイルが別れていても優先度が維持される。
    
    Appleでは謎？別オブジェクトファイルだと優先度通りにならない？

--- libsrc/_wasm/unistd/getcwd.c
+++ libsrc/_wasm/unistd/getcwd.c
@@ -1,12 +1,13 @@
+#include <stdio.h>
 #include "unistd.h"
 #include "errno.h"
 #include "limits.h"
-#include "stdlib.h"  // malloc
+#include "stdlib.h"  // getenv, malloc
 #include "string.h"  // strlen
 
-char __cwd[PATH_MAX] = ".";
+#include "../wasi.h"
 
-#define MIN(a, b)  ((a) < (b) ? (a) : (b))
+char __cwd[PATH_MAX] = ".";
 
 static inline void GETCWD(char *dst, size_t siz) {
   strncpy(dst, __cwd, siz);
@@ -22,4 +23,32 @@ char *getcwd(char *buffer, size_t size) {
   }
   GETCWD(buffer, size);
   return buffer;
+}
+
+__attribute__((constructor))
+static void extract_pwd_from_env(void) {
+  // TODO: Use `getenv` to extract PWD (currently init priority is not guaranteed).
+
+  size_t environ_count = 0;
+  size_t content_size = 0;
+  int r = environ_sizes_get(&environ_count, &content_size);
+  if (r == 0) {
+    void *buf = calloc(1, (environ_count + 1) * sizeof(char*) + content_size);
+    if (buf != NULL) {
+      char **env = buf;
+      char *contents = (char*)buf + (environ_count + 1) * sizeof(char*);
+      r = environ_get(env, contents);
+      if (r == 0) {
+        for (size_t i = 0; i < environ_count; ++i) {
+          const char *p = env[i];
+          if (strncmp(p, "PWD=", 4) == 0) {
+            strncpy(__cwd, p + 4, sizeof(__cwd));
+            __cwd[sizeof(__cwd) - 1] = '\0';  // Ensure null-termination.
+            break;
+          }
+        }
+      }
+    }
+    free(buf);
+  }
 }
--- libsrc/stdlib/environ.c
+++ libsrc/stdlib/environ.c
@@ -12,12 +12,15 @@ static void init_environ(void) {
   size_t content_size = 0;
   int r = environ_sizes_get(&environ_count, &content_size);
   if (r == 0) {
-    char **env = calloc(sizeof(char*), environ_count + 1);
-    char *contents = calloc(1, content_size);
-    r = environ_get(env, contents);
-    if (r == 0) {
-      env[environ_count] = NULL;
-      environ = env;
+    void *buf = calloc(1, (environ_count + 1) * sizeof(char*) + content_size);
+    if (buf != NULL) {
+      char **env = buf;
+      char *contents = (char*)buf + (environ_count + 1) * sizeof(char*);
+      r = environ_get(env, contents);
+      if (r == 0) {
+        env[environ_count] = NULL;
+        environ = env;
+      }
     }
   }
 }
