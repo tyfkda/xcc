c32772fa953f0771b259ec1ad6bcadacf4429a58
tyfkda <tyfkda@gmail.com>
2026-03-12 21:28:00 +0900

    Add more definitions to inttypes.h
    #244

--- include/inttypes.h
+++ include/inttypes.h
@@ -1,27 +1,44 @@
 #pragma once
 
-#define PRId32   "ld"
-#define PRIu32   "lu"
-#define PRIx32   "lx"
+#define PRId32   "d"
+#define PRIu32   "u"
+#define PRIo32   "o"
+#define PRIx32   "x"
+
 #define PRIdPTR  "ld"
+#define PRIuPTR  "lu"
+#define PRIoPTR  "lo"
 #define PRIxPTR  "lx"
+
+#define PRIdMAX  "lld"
+#define PRIuMAX  "llu"
+#define PRIoMAX  "llo"
+#define PRIxMAX  "llx"
 
 #if defined(__ILP32__)
 
-#define PRId64  "lld"
-#define PRIu64  "llu"
-#define PRIx64  "llx"
+#define PRId64   "lld"
+#define PRIu64   "llu"
+#define PRIo64   "llo"
+#define PRIx64   "llx"
 
 #elif defined(__LP64__)
 
-#define PRId64  "ld"
-#define PRIu64  "lu"
-#define PRIx64  "lx"
+#define PRId64   "ld"
+#define PRIu64   "lu"
+#define PRIo64   "lo"
+#define PRIx64   "lx"
 
 #else
 // ?
 #endif
 
-#define PRIdMAX  "lld"
-#define PRIuMAX  "llu"
-#define PRIoMAX  "llo"
+#define PRId8    PRId32
+#define PRIu8    PRIu32
+#define PRIo8    PRIo32
+#define PRIx8    PRIx32
+
+#define PRId16   PRId32
+#define PRIu16   PRIu32
+#define PRIo16   PRIo32
+#define PRIx16   PRIx32
