a7aa21801b5997ffea616ebd9f287f0ba48d824e
tyfkda <tyfkda@gmail.com>
2026-07-12 15:05:48 +0900

    グローバル変数のエイリアス

--- examples/weak_alias.c
+++ examples/weak_alias.c
@@ -1,18 +1,19 @@
 #define weak_alias(old, new) \
   extern __typeof(old) new __attribute__((__weak__, alias(#old)))
 
+static int value = 67;
+
 int dummy(void) {
   return 99;
 }
 
+weak_alias(value, ref);
+
 static int sub(void) {
-  return 123;
+  return ref;
 }
 
 weak_alias(sub, foobar);
-// static int foobar(void) {
-//   return 123;
-// }
 
 int main(void) {
   int (*p)(void) = foobar;
